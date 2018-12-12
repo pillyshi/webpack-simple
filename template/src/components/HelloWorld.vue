e3765f87bb75c363f53fabc473e8216433bda2a9
Hiroyuki Tanaka <pillyshi21@gmail.com>
2018-12-12 20:27:04 +0900

    My initial commit

---
+++ template/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-list>
+        <v-card v-for="item in items">
+          <v-card-title>
+            HelloWorld
+          </v-card-title>
+        </v-card>
+      </v-list>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style{{#sass}} lang="scss"{{/sass}}>
+</style>
